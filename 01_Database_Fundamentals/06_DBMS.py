3b9f9c9f5764480fd50cc5caca8238888dee0814
mirzayasirabdullahbaig07 <yasirabdullah4549@gmail.com>
2025-07-25 19:19:02 +0500

    DBMS File Added

---
+++ 01_Database_Fundamentals/06_DBMS.py
@@ -0,0 +1,35 @@
+#  What is DBMS?
+# A Database Management System (DBMS) is a software system that provides an interface and tools
+# to store, organize, retrieve, and manage data in a database.
+# It acts as an intermediary between the database and the applications or users that access the stored data.
+#
+#  Analogy:
+# Think of the database as the main boss (data holder), and the DBMS as the secretary who manages
+# and organizes everything—giving access only when needed and making sure everything runs smoothly.
+
+#  DBMS Architecture
+# Hardware & Memory → Operating System → Database → DBMS → Users/Applications
+
+#  Key Features of a Database
+# • A database consists of multiple tables.
+# • To operate and manage a database effectively, a DBMS is required.
+
+#  Functions of DBMS
+
+# 1. Data Management
+#    → Store, retrieve, and modify data efficiently.
+
+# 2. Data Integrity
+#    → Maintain accuracy, consistency, and reliability of the data.
+
+# 3. Concurrency
+#    → Allow multiple users to access and modify data at the same time safely.
+
+# 4. Transaction Management
+#    → Ensure that all database operations are completed successfully or not at all (Atomicity).
+
+# 5. Security
+#    → Allow access only to authorized users and ensure data protection.
+
+# 6. Utilities
+#    → Handle tasks like data import/export, user management, backup, and logging.
