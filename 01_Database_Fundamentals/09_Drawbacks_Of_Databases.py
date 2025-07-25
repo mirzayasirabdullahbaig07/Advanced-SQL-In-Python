e3b5ad32d3e4eacb435803de24ec8c34b3452ae6
mirzayasirabdullahbaig07 <yasirabdullah4549@gmail.com>
2025-07-25 20:04:26 +0500

    Database Imp Point File Added

---
+++ 01_Database_Fundamentals/09_Drawbacks_Of_Databases.py
@@ -0,0 +1,29 @@
+# Drawbacks of Databases
+
+# 1. Complexity:
+# Setting up, designing, and maintaining a database can be complex and time-consuming,
+# especially for large-scale systems with many interrelated components.
+
+# 2. Cost:
+# Databases can be expensive due to the cost of hardware, licensed software (like Oracle, SQL Server),
+# skilled personnel (DBAs, developers), maintenance, and infrastructure.
+
+# 3. Scalability Challenges:
+# As the volume of stored data grows, managing and scaling the database efficiently becomes harder.
+# Performance issues may arise if scaling is not handled properly.
+
+# 4. Data Integrity:
+# Maintaining the accuracy and consistency of data is challenging,
+# especially in systems where multiple users are performing concurrent operations.
+
+# 5. Security:
+# Protecting databases from unauthorized access, data breaches, and cyber attacks requires advanced security mechanisms,
+# which can be difficult to implement and manage effectively.
+
+# 6. Data Migration:
+# Migrating data between different database systems or during upgrades can be risky and time-consuming.
+# It often involves compatibility issues and potential data loss or corruption.
+
+# 7. Flexibility Limitations:
+# Databases often follow a predefined schema (especially in relational databases),
+# which can make it hard to adapt to evolving business requirements or to store new types of unstructured data.
